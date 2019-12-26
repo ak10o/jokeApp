6f92f2c170dd3a2679002d2c850825c6283dcca4
Avinash kumar <avinash369kumar@gmail.com>
2019-12-26 22:25:43 +0530

    xml based configuration

--- src/main/java/com/mbrdi/jokesapp/JokesAppApplication.java
+++ src/main/java/com/mbrdi/jokesapp/JokesAppApplication.java
@@ -2,8 +2,10 @@ package com.mbrdi.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("chuck-config.xml")
 public class JokesAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mbrdi/jokesapp/config/ChuckConfiguration.java
+++ src/main/java/com/mbrdi/jokesapp/config/ChuckConfiguration.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Configuration;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
 	// we need to create an object of this since it is external jar or dependency.
-	@Bean
+	//@Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
